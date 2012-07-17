0941be46b5efe2e25279253ef9db77d91c31a774
Jayce53 <Jayce53@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-17 17:08:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easysnippet/trunk@573664 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-easysnippet.php
@@ -0,0 +1,159 @@
+<?php
+/*
+
+Copyright (c) 2010-2012 Box Hill LLC
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+if (!class_exists('EasyTemplate', false)) {
+    require_once dirname(__FILE__) . '/lib/EasyTemplate.php';
+}
+
+class EasySnippet {
+    const JQUERYJS = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
+    const JQUERYUIJS = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js";
+    const JQUERYUICSS = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/themes/base/jquery-ui.css";
+    const GOOGLEURL = 'http://www.google.com/webmasters/tools/richsnippets?url=%s';
+    private $pluginsURL;
+    private $pluginsDIR;
+    private $thisPluginDIR;
+    private $version = '1.0';
+    
+    function __construct() {
+        
+        /*
+         * For convenience
+        */
+        $this->pluginsURL = WP_PLUGIN_URL;
+        $this->pluginsDIR = WP_PLUGIN_DIR;
+        $this->thisPluginDIR = dirname(__FILE__);
+        $this->thisPluginURL = rtrim(plugin_dir_url(__FILE__), '/');
+        
+        add_action('admin_menu', array ($this, 'addMenu'));
+        
+        if ($GLOBALS["pagenow"] == 'admin-ajax.php' && (!isset($_REQUEST['action']) || ($_REQUEST['action'] != 'snippettest') && $_REQUEST['action'] != 'snippetget')) {
+            return;
+        }
+        
+        add_action('wp_ajax_snippettest', array ($this, 'snippetTest'));
+        add_action('wp_ajax_snippetget', array ($this, 'getPosts'));
+    
+    }
+    
+    function enqueueScripts() {
+        wp_enqueue_script('easysnippet', "$this->thisPluginURL/js/easysnippet.js", array ('jquery-ui-tabs', 'jquery-ui-button'), $this->version, true);
+    }
+    
+    function enqueueStyles() {
+        wp_enqueue_style("easysnippetUI", "$this->thisPluginURL/ui/easysnippetUI.css", array (), $this->version);
+        wp_enqueue_style("easysnippet", "$this->thisPluginURL/css/easysnippet.css", array (), $this->version);
+    }
+    
+    function addMenu() {
+        $hook = add_menu_page('Sitewide Rich Snippet Report', 'Rich Snippets', 'manage_options', 'easysnippet', array ($this, 'run'), "$this->pluginsURL/easysnippet/images/snippet16.png");
+        add_action("admin_print_scripts-$hook", array ($this, 'enqueueScripts'));
+        add_action("admin_print_styles-$hook", array ($this, 'enqueueStyles'));
+    }
+    
+    function run() {
+        global $wpdb;
+        $ajaxURL = admin_url('admin-ajax.php');
+        
+        $count = $wpdb->get_results("SELECT COUNT(*) AS c FROM $wpdb->posts WHERE post_status = 'publish'");
+        $count = $count[0]->c;
+        echo <<<EOD
+        <script type="text/javascript">
+        /* <![CDATA[ */
+        if (typeof RICHSNIPPET == "undefined") {
+            var RICHSNIPPET = {
+            };
+        }
+        RICHSNIPPET.pluginsURL = '$this->pluginsURL';
+        RICHSNIPPET.ajaxURL = '$ajaxURL';
+        RICHSNIPPET.totalPosts= $count;
+        /* ]]> */
+        </script>
+EOD;
+        $data = new stdClass();
+        $data->blogName = get_option("blogname");
+        
+        $template = new EasyTemplate("$this->thisPluginDIR/templates/easysnippet.html");
+        $html = $template->replace($data);
+        echo $html;
+    
+    }
+    
+    function getPosts() {
+        global $wpdb;
+        
+        if (!current_user_can("administrator")) {
+            echo "Not admin!";
+            exit();
+        }
+        
+        $last = $_POST['last'];
+        $seq = $_POST['seq'] == 0 ? 'ASC' : 'DESC';
+        $data = array ();
+        $posts = $wpdb->get_results("SELECT ID, post_title FROM $wpdb->posts WHERE ID > $last AND post_status = 'publish' ORDER BY ID $seq LIMIT 5");
+        foreach ($posts as $post) {
+            $item = new stdClass();
+            $item->ID = $post->ID;
+            $item->postURL = get_permalink($post->ID);
+            $item->postTitle = get_the_title($post->ID);
+            $data[] = $item;
+        }
+        echo json_encode($data);
+        exit();
+    }
+    
+    function snippetTest() {
+        $url = $_POST['url'];
+        $id = $_POST['id'];
+        $errno = 0;
+        $errstr = '';
+        $return = new stdClass();
+        
+        $url = sprintf(self::GOOGLEURL, urlencode($url));
+        
+        $ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+        $args = array ('user-agent' => $ua);
+        $response = wp_remote_get($url, $args);
+        
+        if (is_wp_error($response)) {
+            $return->html = "Could not connect to www.google.com";
+            $return->id = $id;
+            $return->status = -1;
+            echo json_encode($return);
+            exit();
+        }
+        
+        $html = $response['body'];
+        $return->html = substr($html, strpos($html, "<body"));
+        $return->id = $id;
+        $return->status = $response['response']['code'];
+        echo json_encode($return);
+        exit();
+    }
+    
+    function easysnippetActivated() {
+        // Hook for activation code
+    }
+    
+    function easysnippetDeactivated() {
+        // Hook for deactivation code
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Rich Snippet Site Report ===
+Contributors: Jayce53
+Tags: Rich Snippet, Google Rich Snippet Test Tool, Recipe rich snippet, product rich snippet, review rich snippet
+Requires at least: 3.2
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+
+Test Rich Snippet markup on all pages and see Google's preview. For all rich snippet markup: recipes, reviews, products, people, music and more 
+
+== Description ==
+
+Run all the pages on your blog through Google's [Rich Snippet Test Tool](http://www.google.com/webmasters/tools/richsnippets) with a single click.
+ 
+When Google implemented [Rich Snippets](http://support.google.com/webmasters/bin/answer.py?hl=en&answer=99170) they 
+also provided a test tool so you can verify that a page is correctly formatted and see how Google will show your page in their search results.
+ 
+That's great, but... if you need to check more than a few pages, it's a time consuming and tedious task.
+ 
+EasySnippet makes it easy to check every page on your Wordpress blog with just one click.
+
+Works for all rich snippet markup: recipes, reviews, products, people, music and more.
+
+Requires PHP 5+
+
+== Installation ==
+
+= Using the WordPress Dashboard =
+
+1. Login in to admin on your blog
+1. Go to *Plugins*
+1. Select *Add New*
+1. Search for "EasySnippet"
+1. Select *Install*
+1. Select *Install Now*
+1. Once the plugin has been uploaded, select *Activate Plugin*
+
+= Manual =
+
+1. Download and unzip the plugin into a local folder
+2. Upload the entire */easysnippet* directory to the *wp-content/plugins/* directory
+3. Activate the plugin through the Plugins menu in WordPress
+
+== Screenshots ==
+1. Click on the *Rich Snippets* item in WP menu
+2. Enter the number of seconds bewteen each test (be nice to Google!) and the sequence to run the test.  Click *Start*
+3. A sample of the results
+
+== Changelog ==
+= 1.0 =
+* First release
